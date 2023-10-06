025b9fb399ff2f6d590bde37e1e0f1c480f73c56
ahmed <ahmedsaheed2@outlook.com>
2023-10-06 08:46:39 +0100

    core(parser): implement parser for `let` statements

---
+++ parser/parser.go
@@ -0,0 +1,97 @@
+package parser
+
+import (
+	"fmt"
+	"monkey/lang-monkey/ast"
+	"monkey/lang-monkey/lexer"
+	"monkey/lang-monkey/token"
+)
+
+type Parser struct {
+	L            *lexer.Lexer
+	currentToken token.Token
+	peekToken    token.Token
+	errors       []string
+}
+
+func (P *Parser) Errors() []string {
+	return P.errors
+}
+
+func New(L *lexer.Lexer) *Parser {
+	p := &Parser{
+		L:      L,
+		errors: []string{},
+	}
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (P *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, P.peekToken.Type)
+	P.errors = append(P.errors, msg)
+}
+
+func (P *Parser) nextToken() {
+	P.currentToken = P.peekToken
+	P.peekToken = P.L.NextToken()
+}
+
+func (P *Parser) ParseProgram() *ast.Program {
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for P.currentToken.Type != token.EOF {
+		stmt := P.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		P.nextToken()
+	}
+	return program
+}
+
+func (P *Parser) parseStatement() ast.Statement {
+	switch P.currentToken.Type {
+	case token.LET:
+		return P.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (P *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: P.currentToken}
+
+	if !P.expectPeek(token.IDENT) {
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{Token: P.currentToken, Value: P.currentToken.Literal}
+
+	if !P.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	//TODO: Skip expressions till encounter semicolons
+
+	for !P.currentTokenLetStatement(token.SEMICOLON) {
+		P.nextToken()
+	}
+	return stmt
+}
+
+func (P *Parser) expectPeek(t token.TokenType) bool {
+	if P.peekTokenLetStatement(t) {
+		P.nextToken()
+		return true
+	} else {
+		P.peekError(t)
+		return false
+	}
+}
+
+func (P *Parser) currentTokenLetStatement(t token.TokenType) bool { return P.currentToken.Type == t }
+func (P *Parser) peekTokenLetStatement(t token.TokenType) bool    { return P.peekToken.Type == t }
---
+++ parser/parser_test.go
@@ -0,0 +1,133 @@
+package parser
+
+import (
+	"monkey/lang-monkey/ast"
+	"monkey/lang-monkey/lexer"
+	"testing"
+)
+
+func TestLetStatements(t *testing.T) {
+	input := `
+let x = 5;
+let y = 10;
+let foobar = 838383;
+`
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+	if program == nil {
+		t.Fatalf("ParseProgram() returned nil")
+	}
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements. got=%d",
+			len(program.Statements))
+	}
+
+	tests := []struct {
+		expectedIdentifier string
+	}{
+		{"x"},
+		{"y"},
+		{"foobar"},
+	}
+
+	for i, tt := range tests {
+		stmt := program.Statements[i]
+		if !testLetStatement(t, stmt, tt.expectedIdentifier) {
+			return
+		}
+	}
+}
+
+//func TestLetStatements(t *testing.T) {
+//	tests := []struct {
+//		input              string
+//		expectedIdentifier string
+//		expectedValue      interface{}
+//	}{
+//		{"let x = 5;", "x", 5},
+//		{"let y = true;", "y", true},
+//		{"let foobar = y;", "foobar", "y"},
+//	}
+//
+//	for _, tt := range tests {
+//		l := lexer.New(tt.input)
+//		p := New(l)
+//		program := p.ParseProgram()
+//		checkParserErrors(t, p)
+//
+//		if len(program.Statements) != 1 {
+//			t.Fatalf("program.Statements does not contain 1 statements. got=%d",
+//				len(program.Statements))
+//		}
+//
+//		stmt := program.Statements[0]
+//		if !testLetStatements(t, stmt, tt.expectedIdentifier) {
+//			return
+//		}
+//
+//		val := stmt.(*ast.LetStatement).Value
+//		if !testLiteralExpression(t, val, tt.expectedValue) {
+//			return
+//		}
+//	}
+//}
+
+func testLiteralExpression(
+	t *testing.T,
+	exp ast.Expression,
+	expected interface{},
+) bool {
+	switch v := expected.(type) {
+	case int:
+		return testIntegerLiteral(t, exp, int64(v))
+	case int64:
+		return testIntegerLiteral(t, exp, v)
+	case string:
+		return testIdentifierLiteral(t, exp, v)
+	case bool:
+		testBooleanLiteral(t, exp, v)
+	}
+	t.Errorf("type of expression not handled. got=%T", exp)
+	return false
+}
+
+func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
+	if s.TokenLiteral() != "let" {
+		t.Errorf("s.tokenLiteral not 'let'. got=%s", s.TokenLiteral())
+		return false
+	}
+	letStmt, ok := s.(*ast.LetStatement)
+	if !ok {
+		t.Errorf("s not *ast.LetStatement. got=%T", s)
+		return false
+	}
+
+	if letStmt.Name.Value != name {
+		t.Errorf("letStmt.Name.Value not '%s'. got=%s", name, letStmt.Name.Value)
+		return false
+	}
+	if letStmt.Name.TokenLiteral() != name {
+		t.Errorf("letStmt.Name.TokenLiteral() not '%s'. got=%s", name, letStmt.Name.TokenLiteral())
+		return false
+	}
+	return true
+}
+func testIntegerLiteral(t *testing.T, expression ast.Expression, val int64) bool     { return false }
+func testIdentifierLiteral(t *testing.T, expression ast.Expression, val string) bool { return false }
+func testBooleanLiteral(t *testing.T, expression ast.Expression, val bool) bool      { return false }
+
+func checkParserErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+	if len(errors) == 0 {
+		return
+	}
+
+	t.Errorf("parser has %d errors", len(errors))
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+	t.FailNow()
+}
