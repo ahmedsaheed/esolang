f278c9ab3d01d8fd702709b2ee9506f353ff6c70
ahmed <ahmedsaheed2@outlook.com>
2024-03-16 04:04:00 +0000

    feat(core): evaluation of closures

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -73,6 +73,17 @@ func TestFunctionApplication(t *testing.T) {
 	}
 }
 
+func TestClosures(t *testing.T) {
+	input := `
+	let newAdder = fn(x) {
+		fn(y) { x + y };
+	};
+	let addTwo = newAdder(2);
+	addTwo(2);
+	`
+	testIntegerObject(t, testEval(input), 4)
+}
+
 func TestEvaluateBooleanExpression(t *testing.T) {
 	tests := []struct {
 		input    string
