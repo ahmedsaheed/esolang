b52c3732ffc1cea5ca1ce02169aa4cfc500595e5
ahmed <ahmedsaheed2@outlook.com>
2024-03-23 05:31:25 +0000

    feat(core): add driver to evaluate files & flag for repl mode

--- esolang.go
+++ esolang.go
@@ -2,17 +2,41 @@ package main
 
 import (
 	"esolang/lang-esolang/repl"
-	"fmt"
+	"flag"
 	"os"
-	"os/user"
+	"path/filepath"
+
+	"github.com/charmbracelet/log"
 )
 
 func main() {
-	user, err := user.Current()
-	if err != nil {
-		panic(err)
+	replMode := flag.Bool("repl", false, "Start the repl")
+	logger := log.New(os.Stderr)
+	flag.Parse()
+
+	if *replMode {
+		repl.Start(os.Stdin, os.Stdout)
 	}
-	fmt.Printf("Howdy %s! Welcome to esolang's repl \n", user.Username)
-	fmt.Printf("Feel free to type in commands \n")
-	repl.Start(os.Stdin, os.Stdout)
+
+	if len(flag.Args()) > 0 {
+		inputFile, err := os.ReadFile(file)
+
+		if err != nil {
+			logger.Error("Error reading file %s", os.Args[1])
+			os.Exit(1)
+		}
+		fileExtension := filepath.Ext(file)
+		if fileExtension != ".eso" {
+			logger.Error("Invalid file extension. Please provide a file with .eso extension")
+			os.Exit(1)
+		}
+
+		repl.Execute(string(inputFile))
+	} else {
+		logger.Warn("No file provided. Please provide a file to run or use the -repl flag to start the repl.")
+		logger.Warn("Usage: esolang <path-to-filename>")
+		logger.Warn("Usage: esolang -repl")
+		os.Exit(1)
+	}
+
 }
--- repl/repl.go
+++ repl/repl.go
@@ -12,20 +12,31 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"os/user"
 
 	"github.com/charmbracelet/log"
 )
 
 const PROMPT = ">>"
 
-// Start starts the Read-Eval-Print-Loop.
+func Execute(input string) {
+	environmnet := object.NewEnvironment()
+	logger := generateLogger()
+	evaluteInput(input, logger, environmnet)
+}
+
 func Start(in io.Reader, out io.Writer) {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! Welcome to esolang's repl \n", user.Username)
+	fmt.Printf("Feel free to type in commands \n")
+	fmt.Printf("Type '.help' for assistance \n")
 	scanner := bufio.NewScanner(in)
 	environmnet := object.NewEnvironment()
-	logger := log.New(os.Stderr)
-	logger.SetReportTimestamp(false)
-	logger.SetReportCaller(false)
-	logger.SetLevel(log.DebugLevel)
+	logger := generateLogger()
+
 	for {
 		fmt.Fprintf(out, PROMPT)
 		scanned := scanner.Scan()
@@ -33,26 +44,53 @@ func Start(in io.Reader, out io.Writer) {
 			return
 		}
 		line := scanner.Text()
-		lexer := lexer.New(line)
-		parser := parser.New(lexer)
-
-		program := parser.ParseProgram()
-
-		if len(parser.Errors()) != 0 {
-			printParserErrors(parser.Errors(), logger)
-			continue
+		if line == ".help" {
+			replHelp()
+		} else if line == ".exit" {
+			fmt.Println("Goodbye!")
+			return
+		} else {
+			evaluteInput(line, logger, environmnet)
 		}
-		evaluated := evaluator.Eval(program, environmnet)
-		if evaluated != nil {
-			message := evaluated.Inspect()
-			if len(message) > 5 && message[:5] == "ERROR" {
-				logger.Error(message[6:])
-			} else {
-				logger.Info(message)
-				io.WriteString(out, "\n")
-			}
+
+	}
+}
+
+func evaluteInput(input string, log *log.Logger, environmnet *object.Environment) {
+	lexer := lexer.New(input)
+	parser := parser.New(lexer)
+
+	program := parser.ParseProgram()
+
+	if len(parser.Errors()) != 0 {
+		printParserErrors(parser.Errors(), log)
+		return
+	}
+	evaluated := evaluator.Eval(program, environmnet)
+	if evaluated != nil {
+		output := evaluated.Inspect()
+		if len(output) > 5 && output[:5] == "ERROR" {
+			log.Error(output[6:])
+		} else {
+			log.Info(output)
 		}
 	}
+}
+
+func generateLogger() *log.Logger {
+	logger := log.New(os.Stderr)
+	logger.SetReportTimestamp(false)
+	logger.SetReportCaller(false)
+	logger.SetLevel(log.DebugLevel)
+
+	return logger
+}
+
+func replHelp() {
+	fmt.Println("Here are a few commands you can use:")
+	fmt.Println(".help - Show this message")
+	fmt.Println(".exit - Exit the repl")
+	fmt.Println("Feel free to type in commands")
 }
 
 func printParserErrors(errors []string, log *log.Logger) {
