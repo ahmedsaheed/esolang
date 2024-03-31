18b78cc7ee2ec688a1fc9b11ad89cab9c9f526fa
ahmed <ahmedsaheed2@outlook.com>
2024-03-31 07:16:47 +0100

    feat(core): make http response a type of hash for index accessibility

--- builtins/http.go
+++ builtins/http.go
@@ -1,12 +1,21 @@
 package builtins
 
 import (
+	"encoding/json"
 	"esolang/lang-esolang/object"
 	"fmt"
 	"io"
 	"net/http"
+	"reflect"
 	"strings"
 )
+
+type Response struct {
+	Body       string
+	StatusCode int
+	Status     string
+	Header     http.Header
+}
 
 func _http(args ...object.Object) object.Object {
 	requestType := args[0].(*object.String).Value
@@ -29,15 +38,11 @@ func _http(args ...object.Object) object.Object {
 		if len(args) == 3 {
 			return newError("GET request should not have a body")
 		}
-		resp, err := http.Get(url)
+		response, err := http.Get(url)
 		if err != nil {
 			return newError("HTTP Error: Error making GET request %s", url)
 		}
-		body, err := io.ReadAll(resp.Body)
-		stringifiedResp := string(body)
-
-		// TODO: We can check if it [Object.Hash] or [Object.Array] and convert it to respective object
-		return &object.String{Value: stringifiedResp}
+		return outputResp(response)
 	}
 
 	outGoingBody := args[2]
@@ -48,16 +53,15 @@ func _http(args ...object.Object) object.Object {
 		}
 		body := outGoingBody.(*object.Hash).Inspect()
 
-		resp, err := http.Post(url, "application/json", strings.NewReader(body))
+		response, err := http.Post(url, "application/json", strings.NewReader(body))
 		if err != nil {
 			return newError("HTTP Error: Error making POST request %s", url)
 		}
-		respBody, err := io.ReadAll(resp.Body)
-		statusCode := fmt.Sprintf(" Status Code: %d", resp.StatusCode)
-		stringifiedResp := string(respBody) + statusCode
-		// TODO: We can check if it [Object.Hash] or [Object.Array] and convert it to respective object
-		return &object.String{Value: stringifiedResp}
 
+		if err != nil {
+			return newError("HTTP Error: could read response body %s", err)
+		}
+		return outputResp(response)
 	}
 
 	if requestType == "PATCH" {
@@ -66,19 +70,16 @@ func _http(args ...object.Object) object.Object {
 		}
 		body := outGoingBody.(*object.Hash).Inspect()
 		client := &http.Client{}
-		req, err := http.NewRequest("PATCH", url, strings.NewReader(body))
+		req, err := http.NewRequest(requestType, url, strings.NewReader(body))
 		if err != nil {
 			return newError("HTTP Error: Error making PATCH request %s", url)
 		}
-		resp, err := client.Do(req)
+		response, err := client.Do(req)
 
 		if err != nil {
 			return newError("HTTP Error: Error making PATCH request %s", url)
 		}
-		respBody, err := io.ReadAll(resp.Body)
-		stringifiedResp := string(respBody)
-		// TODO: We can check if it [Object.Hash] or [Object.Array] and convert it to respective object
-		return &object.String{Value: stringifiedResp}
+		return outputResp(response)
 	}
 
 	return NULL
@@ -91,41 +92,107 @@ func checkBrackets(text string) bool {
 	return text[0] == '[' && text[len(text)-1] == ']'
 }
 
-// covert string to object.Hash
-func convertToHash(text string) *object.Hash {
-	individualObjs := strings.Split(text, "},")
-	finalHash := &object.Hash{Pairs: make(map[object.HashKey]object.HashPair)}
-	text = text[1 : len(text)-1]
+func outputResp(res *http.Response) object.Object {
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		newError("HTTP Error: could read response body %s", err)
+	}
+	stringifiedBody := string(body)
 
-	for _, individualObj := range individualObjs {
-		// get each key value pair
-		replacer := strings.NewReplacer("{", "", "}", "").Replace(individualObj)
-		keyValuePairs := strings.Split(replacer, ",")
-		fmt.Println("Key Value Pairs are", keyValuePairs)
+	resp := Response{
+		Body:       stringifiedBody,
+		StatusCode: res.StatusCode,
+		Status:     res.Status,
+		Header:     res.Header,
+	}
+	// str := fmt.Sprintf("{Body: %s, StatusCode: %d, Status: %s, Header: %v}",
+	// 	resp.Body, resp.StatusCode, resp.Status, resp.Header)
+	fmt.Println("value of hash ", generateHashFromResponse(resp).Inspect())
+	// return &object.String{Value: str}
+	generateResponseBodyHash(resp.Body)
+	return generateHashFromResponse(resp)
+}
 
-		// create a new hash
-		hash := &object.Hash{Pairs: make(map[object.HashKey]object.HashPair)}
-		for _, pair := range keyValuePairs {
-			// get key value pair
-			keyValue := strings.Split(pair, ":")
-			if len(keyValue) != 2 {
-				continue
-			}
-			key := object.String{
-				Value: strings.Trim(keyValue[0], " "),
-			}
-			hashKey := key.HashKey()
+func generateHashFromResponse(res Response) *object.Hash {
+	hash := &object.Hash{Pairs: make(map[object.HashKey]object.HashPair)}
+	headerHash := &object.Hash{Pairs: make(map[object.HashKey]object.HashPair)}
+	// bodyHash := &object.Hash{Pairs: make(map[object.HashKey]object.HashPair)}
 
-			value := strings.Trim(keyValue[1], " ")
-			fmt.Println("Key is ", key, " Value is ", value)
-			hash.Pairs[hashKey] = object.HashPair{Key: &object.String{Value: key.Value}, Value: &object.String{Value: value}}
-
-			finalHash.Pairs[hashKey] = object.HashPair{Key: &object.String{Value: key.Value}, Value: hash}
-
-		}
-
+	for key, value := range res.Header {
+		keyValue := (&object.String{Value: key}).HashKey()
+		hashKey := object.HashKey{Type: object.STRING_OBJ, Value: keyValue.Value}
+		headerHash.Pairs[hashKey] = object.HashPair{Key: &object.String{Value: key}, Value: &object.String{Value: value[0]}}
 	}
 
-	fmt.Println("Converted Hash is ", finalHash.Inspect())
-	return finalHash
+	headerKey := (&object.String{Value: "Header"}).HashKey()
+	hashKey := object.HashKey{Type: object.STRING_OBJ, Value: headerKey.Value}
+	hash.Pairs[hashKey] = object.HashPair{Key: &object.String{Value: "Header"}, Value: headerHash}
+
+	statusCodeKey := (&object.String{Value: "StatusCode"}).HashKey()
+	hashKey = object.HashKey{Type: object.STRING_OBJ, Value: statusCodeKey.Value}
+	hash.Pairs[hashKey] = object.HashPair{Key: &object.String{Value: "StatusCode"}, Value: &object.Integer{Value: int64(res.StatusCode)}}
+
+	statusKey := (&object.String{Value: "Status"}).HashKey()
+	hashKey = object.HashKey{Type: object.STRING_OBJ, Value: statusKey.Value}
+	hash.Pairs[hashKey] = object.HashPair{Key: &object.String{Value: "Status"}, Value: &object.String{Value: res.Status}}
+
+	// populate bodyHash
+
+	bodyKey := (&object.String{Value: "Body"}).HashKey()
+	bodyHash := generateResponseBodyHash(res.Body)
+	bodyHashKey := object.HashKey{Type: object.STRING_OBJ, Value: bodyKey.Value}
+	hash.Pairs[bodyHashKey] = object.HashPair{Key: &object.String{Value: "Body"}, Value: bodyHash}
+
+	return hash
+}
+
+func generateResponseBodyHash(body string) *object.Hash {
+	// VERY MUCH WIP
+	hash := &object.Hash{Pairs: make(map[object.HashKey]object.HashPair)}
+
+	if checkBrackets(body) {
+		// parse the string as a json array
+		var arr []interface{}
+		err := json.Unmarshal([]byte(body), &arr)
+		if err != nil {
+			fmt.Println("error parsing json object")
+			return &object.Hash{Pairs: make(map[object.HashKey]object.HashPair)}
+		}
+		for i, v := range arr {
+			hashValueKey := (&object.Integer{Value: int64(i)}).HashKey()
+			hashKey := object.HashKey{Type: object.INTEGER_OBJ, Value: hashValueKey.Value}
+			hash.Pairs[hashKey] = object.HashPair{Key: &object.Integer{Value: int64(i)}, Value: &object.String{Value: fmt.Sprintf("%v", v)}}
+		}
+		fmt.Println("value of hash arrayy ", hash.Inspect())
+		return hash
+	}
+
+	// parse the string as a json object
+	var obj map[string]interface{}
+	err := json.Unmarshal([]byte(body), &obj)
+	if err != nil {
+		fmt.Println("error parsing json object")
+		return &object.Hash{Pairs: make(map[object.HashKey]object.HashPair)}
+	}
+	for k, v := range obj {
+		hashValueKey := (&object.String{Value: k}).HashKey()
+		hashKey := object.HashKey{Type: object.STRING_OBJ, Value: hashValueKey.Value}
+		fmt.Println("value of k1 ", k)
+		// nested hash are currently converted to string - let fix this
+
+		if reflect.TypeOf(v).Kind() == reflect.Map {
+			for k1, v1 := range v.(map[string]interface{}) {
+				innerHash := &object.Hash{Pairs: make(map[object.HashKey]object.HashPair)}
+				hashValueKey := (&object.String{Value: k1}).HashKey()
+				hashKey := object.HashKey{Type: object.STRING_OBJ, Value: hashValueKey.Value}
+				innerHash.Pairs[hashKey] = object.HashPair{Key: &object.String{Value: k1}, Value: &object.String{Value: fmt.Sprintf("%v", v1)}}
+				hash.Pairs[hashKey] = object.HashPair{Key: &object.String{Value: k}, Value: innerHash}
+
+			}
+		} else {
+			hash.Pairs[hashKey] = object.HashPair{Key: &object.String{Value: k}, Value: &object.String{Value: fmt.Sprintf("%v", v)}}
+		}
+	}
+	fmt.Println("value of hash hashable ", hash.Inspect())
+	return hash
 }
