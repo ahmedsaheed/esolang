b9e171b5b4a3fc11407b8ef9973d9cb547344ce8
ahmed <ahmedsaheed2@outlook.com>
2024-04-23 01:59:26 +0100

    chore(chore): test gh raw data

---
+++ views/playground-data.js
@@ -0,0 +1,10 @@
+var playGroundSamples = {
+  helloWord: {
+    tutorialHtml: `<p>Write a program that prints "Hello, World!" to the console.</p>`,
+    esolangCode: playGroundSampleEsolangCode.helloWord,
+  },
+};
+
+var playGroundSampleEsolangCode = {
+  helloWord: `print("Hello, World!")`,
+};
